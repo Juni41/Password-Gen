49413fedbaa0e5e8eba3c3c2912f081e8d69b763
Justin Hsiao <158727277+Juni41@users.noreply.github.com>
2024-08-18 15:21:35 +1200

    imported tkinter

--- main.py
+++ main.py
@@ -1,6 +1,9 @@
 import random
 import string
+import tkinter as tk
 
+root = tk.Tk()
+root.mainloop()
 def generate_password(length = 12):
     characters = string.ascii_letters + string.digits + string.punctuation
     password = ''.join(random.choice(characters)) for i in range(length):
