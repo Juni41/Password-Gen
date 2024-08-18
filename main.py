ae13c62702e8c7e68478f40e8cf0e1996e6ae1de
Justin Hsiao <158727277+Juni41@users.noreply.github.com>
2024-08-18 21:55:35 +1200

    added title to tkinter window

--- main.py
+++ main.py
@@ -3,10 +3,17 @@ import string
 import tkinter as tk
 
 root = tk.Tk()
+root.title('Password Generator')
 root.mainloop()
+
+
 def generate_password(length = 12):
     characters = string.ascii_letters + string.digits + string.punctuation
-    password = ''.join(random.choice(characters)) for i in range(length):
+    password = ''.join(random.choice(characters) for i in range(length))
     return password
-    
-    
+
+
+password_length = int(input("Enter the length of the password: "))
+
+password = generate_password(password_length)
+print(f"Generated Password: {password}")
